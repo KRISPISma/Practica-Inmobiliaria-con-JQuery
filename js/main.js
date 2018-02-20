a63ba815c2bc6fbe50bec8a76f28bdbffd9d8fc7
KRISPISma <cristinaflorescarballo@gmail.com>
2018-02-20 20:05:27 +0100

    Menú Hamburguesa resto pag

--- js/main.js
+++ js/main.js
@@ -1,32 +1,32 @@
 $(document).ready(function(){
-    // Menú Hamburguesa
+    // ---------------------------Menú Hamburguesa
     $(".burger").click(function(){
         $(".menu").animate({width:"toggle"},500);
     });
     $(".cerrar").click(function(){
         $(".menu").animate({width:"toggle"},500);
     });
-    // Menú Hamburguesa "2"
+    // ---------------------------Menú Hamburguesa "2"
     $(".burger2").click(function(){
         $(".burger2").toggleClass("abierto");
         $(".menu2").animate({width:"toggle"},500);
         $(".menu2 a").toggleClass("aparicion");
     });
-    // Cambio de estilo de header
-    $(window).on("scroll",function(){
-        var posicion = $("#primera").offset();
-        // console.log(posicion);
-        if ($(window).scrollTop() > posicion.top -50){
-            $("header").addClass("normal");
-            $(".logo-normal").show();
-            $(".logo-blanco").hide();
-        } else {
-            $("header").removeClass("normal");
-            $(".logo-normal").hide();
-            $(".logo-blanco").show();
-        }
-    });
-    // Scroll suave
+    // // -----------------------------------Cambio de estilo de header
+    // $(window).on("scroll",function(){
+    //     var posicion = $("#primera").offset();
+    //     // console.log(posicion);
+    //     if ($(window).scrollTop() > posicion.top -50){
+    //         $("header").addClass("normal");
+    //         $(".logo-normal").show();
+    //         $(".logo-blanco").hide();
+    //     } else {
+    //         $("header").removeClass("normal");
+    //         $(".logo-normal").hide();
+    //         $(".logo-blanco").show();
+    //     }
+    // });
+    // ------------------------------Scroll suave
     $("a").on("click",function(){
         if(this.hash !== ""){
             var hash = this.hash;
@@ -35,22 +35,22 @@ $(document).ready(function(){
             },800);
         }
     });
-    // Pestañas
-    $(".pestanyas li").click(function(){
-        var pest_id = $(this).attr("pest");
-        // console.log(pes_id);
-        $(".pestanyas li").removeClass("pulsada");
-        $(".contenido").removeClass("mostrado");
+    // // ------------------------------------------Pestañas
+    // $(".pestanyas li").click(function(){
+    //     var pest_id = $(this).attr("pest");
+    //     // console.log(pes_id);
+    //     $(".pestanyas li").removeClass("pulsada");
+    //     $(".contenido").removeClass("mostrado");
         
-        $(this).addClass("pulsada");
-        $("#"+pest_id).addClass("mostrado");
-    });
-    // Menú 3
-    $(".btn-menu3").click(function(){
-        $(".menu3").animate({width:"toggle"},500);
-        $(".btn-menu3 span").toggleClass("girada");
-    });    
-    // Ventana modal
+    //     $(this).addClass("pulsada");
+    //     $("#"+pest_id).addClass("mostrado");
+    // });
+    // // Menú 3
+    // $(".btn-menu3").click(function(){
+    //     $(".menu3").animate({width:"toggle"},500);
+    //     $(".btn-menu3 span").toggleClass("girada");
+    // });    
+    // ----------------------------------Ventana modal
     $(".open").click(function(){
         $(".modal, .overlay").addClass("visible");
     });
